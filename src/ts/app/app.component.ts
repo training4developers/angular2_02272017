338f7fb73b7ec2a87e35519cc892900c1ddb75fb
Eric W. Greene <eric@ericwgreene.com>
2017-02-27 13:55:33 -0500

    added code demos

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -9,15 +9,31 @@ import { Component } from "@angular/core";
                 <li *ngFor="let color of colors">{{color}}</li>
             </ul>
         </div>
+        <form>
+            <div>
+                <label for="new-color-input">New Color</label>
+                <input type="text" [(ngModel)]="newColor"
+                    id="new-color-input" name="newColorInput">
+            </div>
+            <button type="button" (click)="addColor()" >
+                Add Color
+            </button>
+        </form>
     `,
 })
 export class AppComponent {
 
     public header: string = "Color Tool!";
+    public newColor: string = "";
 
-    public colors: string[] = [
+    public colors: any[] = [
         "saffron", "green", "white", "red", "gold", "blue",
         "yellow",
     ];
+
+    public addColor() {
+        this.colors.push(this.newColor);
+        this.newColor = "";
+    }
 
 }
