e83805e5891ce145aa865c865d61d3f3504c88b0
Eric W. Greene <eric@ericwgreene.com>
2017-02-27 11:40:22 -0500

    added ngfor code demo

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,22 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<h1>{{header}}</h1>",
+    template: `
+        <div>
+            <h1>{{header}}</h1>
+            <ul>
+                <li *ngFor="let color of colors">{{color}}</li>
+            </ul>
+        </div>
+    `,
 })
 export class AppComponent {
 
     public header: string = "Color Tool!";
+
+    public colors: string[] = [
+        "saffron", "green", "white", "red", "gold", "blue",
+        "yellow",
+    ];
 
 }
